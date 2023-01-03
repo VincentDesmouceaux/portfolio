f8ffa7f8f099b693d20d9fc2972a0a615e919dab
vincent desmouceaux <vincentdesmont@MacBook-Pro-de-vincent.local>
2023-01-03 15:15:48 +0100

    gif

--- src/App.js
+++ src/App.js
@@ -5,16 +5,26 @@ function App() {
     <div>
       <header>
         <h1>Hello World :D !</h1>
+        <div className="helloworld">
+          <img
+            src="https://media.giphy.com/media/bGgsc5mWoryfgKBx1u/giphy.gif"
+            alt="hello"
+          />
+        </div>
       </header>
       <main className="main-block">
         <nav>
           <a href="#about-me">About me</a>
-          <a href="#formation">Formation</a>
-          <a href="#projets">Projects</a>
-          <a href="#formation">Contact</a>
+          <a href="#education">Education</a>
+          <a href="#projects">Projects</a>
+          <a href="#contact">Contact</a>
         </nav>
 
         <img className="computer" src="img/1671641382486.jpg" alt="hello" />
+        <section id="about-me"></section>
+        <section id="education"></section>
+        <section id="projects"></section>
+        <section id="contact"></section>
       </main>
       <footer></footer>
     </div>
