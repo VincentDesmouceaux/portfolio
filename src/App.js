2c83f2fe3dcc5d81702a94b5d0ddd6de01bd79be
vincent desmouceaux <vincentdesmont@MacBook-Pro-de-vincent.local>
2023-01-03 13:51:07 +0100

    image

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           <a href="#formation">Contact</a>
         </nav>
 
-        <img className="computer" src="img/1672746090084.jpg" alt="hello" />
+        <img className="computer" src="img/1671641382486.jpg" alt="hello" />
       </main>
       <footer></footer>
     </div>
