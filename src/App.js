34d2f2c652ca40bb37399f3716699f0c2cb35263
vincent desmouceaux <vincentdesmont@MacBook-Pro-de-vincent.local>
2023-01-04 12:42:41 +0100

    techno

--- src/App.js
+++ src/App.js
@@ -41,7 +41,66 @@ function App() {
             ).
           </p>
         </section>
-        <section id="education"></section>
+        <section id="education">
+          <h2>Education</h2>
+          <p>The technologies and tools on which I continually train 🚀 :</p>
+          <div>
+            <img src="img/javascript.png" alt="js" />
+            <img src="img/html.png" alt="html" />
+            <img src="img/css.png" alt="css" />
+            <img src="/img/react.png" alt="react" />
+            <img src="/img/mongodb.png" alt="react" />
+            <img
+              src="https://raw.githubusercontent.com/jsx-ir/logo/master/jsx.png "
+              alt="JSX"
+            />
+
+            <img
+              src="https://logos-download.com/wp-content/uploads/2020/06/Postman_Logo.png"
+              alt="postman"
+            />
+
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/d/d1/Axios_%28computer_library%29_logo.svg "
+              alt="AXIOS"
+            />
+            <img
+              src="https://res.cloudinary.com/practicaldev/image/fetch/s--H6L3JDpH--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://repository-images.githubusercontent.com/65750241/79017180-d4ce-11e9-9955-3f0a7be00c7a"
+              alt="expo"
+            />
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/langfr-1024px-Node.js_logo.svg.png"
+              alt="node.js"
+            />
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/b/b8/Netlify_logo.svg"
+              alt="netlify"
+            />
+            <img
+              src="https://www.soat.fr/assets/images/formation/git.jpg"
+              alt="git"
+            />
+            <img
+              src="https://sofacto.force.com/support/servlet/rtaImage?eid=ka008000000dkuz&feoid=00N1p00000JI0JP&refid=0EM08000007PCaC"
+              alt="stripe"
+            />
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/b/b0/Cloudinary_logo_blue_0720_2x.png"
+              alt="cloudinary"
+            />
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/1200px-Visual_Studio_Code_1.35_icon.svg.png"
+              alt="vsc"
+            />
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/9/92/Android_Studio_Trademark.svg"
+              alt="astudio"
+            />
+            <img src="" alt="" />
+            <img src="" alt="" />
+          </div>
+        </section>
+
         <section id="projects"></section>
         <section id="contact"></section>
       </main>
